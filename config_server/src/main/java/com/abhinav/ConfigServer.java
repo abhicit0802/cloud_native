fdc78693a6d8981f1a7ca3a1382949883bab783d
Abhinav Tiwary <abhicit0802@yahoo.com>
2018-08-26 13:05:37 +0530

    Added config server to discovery

--- config_server/src/main/java/com/abhinav/ConfigServer.java
+++ config_server/src/main/java/com/abhinav/ConfigServer.java
@@ -1,11 +1,13 @@
 package com.abhinav;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServer {
     public static void main(String[] args) {
