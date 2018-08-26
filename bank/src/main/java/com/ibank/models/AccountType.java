334430c94e5c7a26268c8c81c5c3f4636c37021b
Abhinav Tiwary <abhicit0802@yahoo.com>
2018-08-26 12:16:58 +0530

    Search service initial configuration

--- bank/src/main/java/com/ibank/models/AccountType.java
+++
@@ -1,5 +0,0 @@
-package com.ibank.models;
-
-public enum AccountType {
-    SAVINGS, CURRENT, LOAN
-}
--- bank/src/main/java/com/ibank/services/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.ibank.services;
-
-
-import com.ibank.models.Account;
-
-public interface AccountService {
-    Account create(Account account);
-
-}
